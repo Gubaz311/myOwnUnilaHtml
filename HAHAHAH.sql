86f1087256b4ea7dea79ddf64eee17a1b0710ebb
Gubaz311 <bagusste25@gmail.com>
2025-06-17 10:19:16 +0700

    fix: exclude files

--- HAHAHAH.sql
+++
@@ -1,8 +0,0 @@
-UPDATE mahasiswa
-SET "jenisKelamin" = CASE
-    WHEN npm = '1816021045' THEN 'Perempuan'
-    WHEN npm = '1816031033' THEN 'Perempuan'
-    WHEN npm = '1857051013' THEN 'Laki-Laki'
-
-END
-WHERE npm IN ('1816021045', '1816031033', '1857051013');
